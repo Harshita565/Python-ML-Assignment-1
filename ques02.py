701cb441fed4c2db81c637fba0c1dfeae4c3139e
Harshita565 <harshitabharti565@gmail.com>
2024-06-18 20:33:53 +0530

    Add files via upload

---
+++ ques02.py
@@ -0,0 +1,7 @@
+# Python Programming Assignment-1
+# 2. Write a python program that checks whether a given number is even or odd.
+a= 1001
+if a%2==0:
+    print("It is an even number")
+else:
+    print("It is an odd number")
